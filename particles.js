f7f333bbd2a5eeecb0ef2cbeb8cff47415f0fa44
SHUBHAM SINGH <94193635+ShubhamSingh54@users.noreply.github.com>
2023-08-07 09:20:22 +0530

    Add files via upload

---
+++ particles.js
@@ -0,0 +1,38 @@
+let username=document.getElementById("username");
+let password=document.getElementById("password");
+let flag1=1,flag2=1;
+function validateform(){
+    if(username.value == "")
+    {
+        document.getElementById("usererror").innerHTML="user name is  empty";
+        flag1=0;
+        
+    }
+    else if(username.value.length <3){
+        document.getElementById("usererror").innerHTML="user name minimum 3 char";
+        flag1=0;
+
+    }
+    else{
+        document.getElementById("usererror").innerHTML="";
+        flag=1;
+
+    }
+    if(password.value == "")
+    {
+        document.getElementById("passworderror").innerHTML="password  is  empty";
+        flag2=0;
+    }
+    else{
+        document.getElementById("passworderror").innerHTML="";
+        flag2=1;
+
+    }
+    if(flag1&&flag2){
+        return true;
+    }
+    else{
+        return false;
+    }
+    
+}
